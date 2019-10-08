79d146ab2242a6f19b5865f7c8b859e490482f45
AckDev Development <help@ackdev.com>
2019-10-07 20:49:42 -0400

    Working on ramdisk

--- distrib/installSecureWall-raspbian.sh
+++ distrib/installSecureWall-raspbian.sh
@@ -141,4 +141,42 @@ fi
 
 read -rsp $'Press any key to restart or CTRL-c to abort...note may take 10 minutes to load virus and malware definitions' -n1 key
 shutdown -r now
+exit
+if cat /etc/fstab | grep "ramdisk" ; then
+     echo "RAM disk already exists"
+else
+	echo "Adding RAM disk"
+	cp -r /etc/fstab /etc/fstab --backup=numbered
+	#create fstab entries
+	echo "tmpfs  /mnt/ramdisk  tmpfs  rw,size=512M  0   0" >>/etc/fstab
+	mount -t tmpfs -o size=512m myramdisk /mnt/ramdisk
+#setup sync service
+cat > /lib/systemd/system/ramdisk-sync.service <<'endmsg1'
+[Unit]
+Before=umount.target
+
+[Service]
+Type=oneshot
+User=root
+ExecStartPre=/bin/chown -Rf root:syslog /mnt/ramdisk
+ExecStart=/usr/bin/rsync -ar /mnt/persist_ramdisk/ /mnt/log_ramdisk
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+
+endmsg1
+
+	systemctl enable ramdisk-sync.service
+	
+	oldDir=/var/log
+	pRamDir=/mnt/log_persist_ramdisk
+	ramDir=/mnt/log_ramdisk
+	destDir=echo "${oldDir%/*}"
+	
+	mkdir -p $pRamDir$destDir;mv $oldDir $pRamDir/$oldDir;mkdir $oldDir;mount --bind $oldDir $pRamDir/$oldDir
+fi
+
+
+
  
